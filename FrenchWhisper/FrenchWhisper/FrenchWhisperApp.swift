5452e5bc50ce6ae6db63f4806e9ef9a28dc78988
soldoutbudokan <soldoutbudokan@protonmail.com>
2026-01-28 22:44:45 -0500

    live french translator

---
+++ FrenchWhisper/FrenchWhisper/FrenchWhisperApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct FrenchWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+        .defaultSize(width: 800, height: 500)
+    }
+}
